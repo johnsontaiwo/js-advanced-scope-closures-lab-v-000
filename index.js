b9efec36ed6366365711df6c84c90a400aaa120b
johnsontaiwo <josseytee@gmail.com>
2017-10-20 19:49:33 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function produceDrivingRange(range) {
+  return function(startBlock, endBlock) {
+    let distancetravelled = parseInt(endBlock ) - parseInt(startBlock)
+    if (distancetravelled > range) {
+      return `${distancetravelled - range} blocks out of range`
+    }else {return `within range by ${range - distancetravelled}`
+    }
+  }
+}
+
+function produceTipCalculator(tipCalculator) {
+  return function(fare) {
+    return fare / (tipCalculator * 100) 
+  }
+}
+
+function createDriver() {
+  let DriverId = 0
+
+  return class {
+    constructor (name) {
+      this.name = name
+      this.id = ++DriverId
+    }
+  }
+}
